0e0fcc653ed4ac439efabb05f5c25cf4c05f8ead
alok7139 <alokgarg7139@gmail.com>
2024-08-14 17:54:39 +0530

    commit

--- client/src/components/Donate.jsx
+++ client/src/components/Donate.jsx
@@ -22,7 +22,7 @@ function Donate() {
               value={amount}
               onChange={(e) => setamount(e.target.value)}
               placeholder="Enter Donation Amount (INR)"
-              className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-pink-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2  focus:ring-pink-500"
             />
             <input
               type="text"
