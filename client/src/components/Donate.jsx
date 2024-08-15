cf1c1ced1fadebf0bd38390c9dd893da05e87fc9
alok7139 <alokgarg7139@gmail.com>
2024-08-16 01:06:05 +0530

    commit

--- client/src/components/Donate.jsx
+++ client/src/components/Donate.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import ProgressBar from 'react-scroll-progress-bar';
+import axios from "axios";
 
 function Donate() {
   const [name, setname] = useState('');
@@ -7,14 +8,47 @@ function Donate() {
   const [message, setmessage] = useState('');
   const [amount, setamount] = useState('');
   const [disablebtn, setdisablebtn] = useState(false);
-  const [theme, settheme] = useState('')
+  
+  const handlesubmit = async(e) => {
+     e.preventDefault();
+
+     try {
+      setdisablebtn(true);
+      const {data: {key}} = await axios.get('http://localhost:3000/api/getkey')
+
+      const {data:{order}} = await axios.post('http://localhost:3000/api/v1/checkout' , {amount , name,email,message});
+
+      const options = {
+        key, 
+        amount: order.amount, 
+        currency: "INR",
+        name: "ALok Garg", 
+        description: "Test Transaction",
+        image: "https://www.ntc.edu/sites/default/files/styles/full_width_16_9/public/2021-06/software-development-specialist.jpg?itok=D8qgVwxb",
+        order_id: order.id, 
+        callback_url: "http://localhost:3000/api/v1/payment/verified",
+        prefill: { 
+            name: "Gaurav Kumar", 
+            email: "gaurav.kumar@example.com",
+            contact: "9000090000" 
+        },
+        notes: {
+            "address": "Razorpay Corporate Office"
+        },
+        theme: {
+            "color": "#121212"
+        }
+    };
+    const razor = new window.Razorpay(options);
+    razor.open();
+      
+     } catch (error) {
+        console.error(error);
+        setdisablebtn(false);
+     }
 
 
-  useEffect(() => {
-    const gettheme = window.localStorage.getItem("theme");
-    settheme(gettheme);
-    
-  })
+  }
 
   return (
     <>
@@ -24,7 +58,7 @@ function Donate() {
           <div className="mb-4">
             <img src="/logo.png" alt="logo" className="max-w-xs" />
           </div>
-          <form className="w-full space-y-4">
+          <form className="w-full space-y-4" onSubmit={handlesubmit}>
             <input
               type="number"
               value={amount}
--- client/src/components/PaymentSuccess.jsx
+++ client/src/components/PaymentSuccess.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useSearchParams } from 'react-router-dom'
 
 function PaymentSuccess() {
+
+    const searchquery = useSearchParams()[0];
+
+     const reference = searchquery.get('reference')
   return (
     <div>
-      
+      <h1>order successfully</h1>
+      <p>{reference}</p>
     </div>
   )
 }
