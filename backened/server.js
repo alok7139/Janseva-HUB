755f3dad8955888788bf16655416f641b5463f72
alok7139 <alokgarg7139@gmail.com>
2024-08-16 19:11:23 +0530

    commit

--- backened/server.js
+++ backened/server.js
@@ -1,10 +1,15 @@
 import app from './app.js'
 import Razorpay from 'razorpay'
+import { message } from './contoller/messagecontroller.js';
 
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_API_KEY,
     key_secret: process.env.RAZORPAY_API_SECRET,
   });
+
+app.get("/" , (req,res) => {
+  res.json({message: "server is running"})
+})  
 
 
 app.listen(process.env.PORT , () => {
